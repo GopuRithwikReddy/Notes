afa763499fa1be37434378d746e542a60c995f3e
GopuRithwikReddy <rithwikgopu08@gmail.com>
2026-07-09 08:29:59 +0530

    Prepare app for production deployment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,10 +9,15 @@ const app = express()
 
 app.use(express.json())
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://your-vercel-app.vercel.app",
+        ],
+        credentials: true,
+    })
+);
 
 app.use(cookieParser())
 
